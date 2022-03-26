89140323870161db0928b6e79c027cf78600724f
unknown <you@example.com>
2022-03-25 22:58:52 -0300

    first commit

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { createContext, useState, useEffect } from "react"
+
+// Crear el Context
+export const CategoriasContext = createContext();
+
+// Provider es donde se encuentran las funciones y state
+const CategoriasProvider = (props) => {
+
+    // Crear el state de Context
+    const [categorias, guardarCategorias] = useState([]);
+
+    // Ejecutar el llamado a la API
+    useEffect(() => {
+        const obtenerCategorias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+
+            const categorias = await axios.get(url);
+
+            guardarCategorias(categorias.data.drinks);
+        }
+        obtenerCategorias();
+    }, []);
+
+    return (
+        <CategoriasContext.Provider
+            value={{
+                categorias
+            }}
+        >
+            {props.children}
+        </CategoriasContext.Provider>
+    )
+}
+
+export default CategoriasProvider;
